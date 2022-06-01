2c7f25e620cea886de7f14f280c3b0b2392ae909
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2022-06-01 20:23:37 +0700

    text: learn how to print and variable

---
+++ bin/tipe_data.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /* 
+  Declare variable first with type, but not assign value 
+  the best practice is type of data and name of variable 
+  example:  String fullName; is correct (cammelCase) 
+  */
+  String name;
+  name = "Muhammad Zulfan Wahyudin";
+
+  print(name);
+}
